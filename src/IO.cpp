b308d43aa5d4428f5ec1f4a1c2f79d3ed2cda9b2
Doug Guzman <swiftshadow34@gmail.com>
2018-01-17 20:16:59 -0600

    Adds a class to manage all sensors, motors, and actuators

---
+++ src/IO.cpp
@@ -0,0 +1,18 @@
+/*
+ * IO.cpp
+ *
+ *  Created on: Jan 17, 2018
+ *      Author: 1800855
+ */
+
+#include <IO.h>
+
+IO::IO() {
+	// TODO Auto-generated constructor stub
+
+}
+
+IO::~IO() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ src/IO.h
@@ -0,0 +1,21 @@
+/*
+ * IO.h
+ *
+ *  Created on: Jan 17, 2018
+ *      Author: 1800855
+ */
+
+#ifndef SRC_IO_H_
+#define SRC_IO_H_
+
+/*
+ * Contains an instance of all sensors, motors, and actuators to be passed
+ * throughout the entire program
+ */
+class IO {
+public:
+	IO();
+	virtual ~IO();
+};
+
+#endif /* SRC_IO_H_ */
