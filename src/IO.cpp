212b85498232de42cf6727680b494c1d4e52de01
Lunar Dust <infinitycreeper1@gmail.com>
2018-01-19 16:36:50 -0600

    Patches all current fatal errors and adds opencv

--- src/IO.cpp
+++ src/IO.cpp
@@ -5,24 +5,5 @@
  *      Author: 1800855
  */
 
-#include <IO.h>
-
-IO::IO() {
-	// TODO Auto-generated constructor stub
-
-}
-void IO::Initialize() {
-	if (_instance == NULL) {
-		_instance = new IO();
-		/* put all other instanciations here */
-	}
-}
-
-IO *IO::GetInstance() {
-	return _instance;
-}
-
-IO::~IO() {
-	// TODO Auto-generated destructor stub
-}
+#include "IO.h"
 
