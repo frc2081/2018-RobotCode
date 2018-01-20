2a826acec45888c4f4b2729ec7e72a0470f6737c
Kyle Li <xiuzhi.li@pndhs.org>
2018-01-20 15:37:45 -0600

    Adds cube state machine

--- src/CubeSystem/IntakeStateMachine.cpp
+++ src/CubeSystem/IntakeStateMachine.cpp
@@ -5,28 +5,45 @@
  *      Author: Kyle Li
  */
 
-
-state = 0;
-switch(state) {
-case 0:
-	if(arm sensor == 1){
-		state = 1
-	}
-	else {
-		state = 0
-	}
-case 1:
-	close hte arm;
-	turn the motor inward;
-	if(shooter sensor == 1){
-		state = 2
-	}
-	else {
-		state = 1
-	}
-case 2:
-	stop motor;
-	apen arms;
+	switch(state){
+case 0 :
+if(user_intake_EN == 1){
+	state = 1;
+	arm_CL = 0;
+	intake_En = 0;
+	shooter_EN = 0;
+}
+if(user_intake_EN == 0){
+	state = 0;
+	arm_CL = 0;
+	intake_EN = 0;
+	shooter_EN = 0;
+}
+break;
+case 1 :
+if((cube_NB == 1)&&(user_intake_EN == 1)){
+	state = 2;
+	arm_CL = 1;
+	intake_EN = 1;
+	shooter_EN = 1;
+}
+if((cube_NB == 0)&&(user_intake_EN == 1)){
+	state = 1;
+	arm_CL = 0;
+	intake_EN = 0;
+}
+break;
+case 2 :
+if(user_intake_EN == 0){
+	arm_CL = 0;
+	intake_EN = 0;
+	shooter_EN = 0;
 	state = 0;
 }
-
+if((user_intake_EN == 1)&&(cube_DE)){
+	state = 2;
+	arm_CL = 1;
+	intake_EN = 1;
+}
+break;
+	}
