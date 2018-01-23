2c00d984b3d8bd6a60b504b6e52efcb4a829bccf
1901319 <1901319@192.168.3.152>
2018-01-22 19:07:34 -0600

    instantiated the motors

--- src/IO.h
+++ src/IO.h
@@ -36,22 +36,24 @@ public:
 
 
 	WPI_VictorSPX *drvlb = new WPI_VictorSPX(0);
-	WPI_VictorSPX *drvrb;
-	WPI_VictorSPX *drvlf;
-	WPI_VictorSPX *drvrf;
-	WPI_VictorSPX *intakel;
-	WPI_VictorSPX *intaker;
-	WPI_VictorSPX *shooterl;
-	WPI_VictorSPX *shooterr;
-	WPI_VictorSPX *shooterangl;
-	WPI_VictorSPX *shooterangr;
-	Compressor *compressor;
-	Solenoid *solenoidrampl;
-	Solenoid *solenoidrampr;
-	WPI_VictorSPX *turnlb;
-	WPI_VictorSPX *turnrb;
-	WPI_VictorSPX *turnlf;
-	WPI_VictorSPX *turnrf;
+	WPI_VictorSPX *drvrb  = new WPI_VictorSPX(1);
+	WPI_VictorSPX *drvlf  = new WPI_VictorSPX(2);
+	WPI_VictorSPX *drvrf  = new WPI_VictorSPX(3);
+	WPI_VictorSPX *intakel = new WPI_VictorSPX(4);
+	WPI_VictorSPX *intaker = new WPI_VictorSPX(5);
+	WPI_VictorSPX *shooterl = new WPI_VictorSPX(6);
+	WPI_VictorSPX *shooterr = new WPI_VictorSPX(7);
+	WPI_VictorSPX *shooterangl = new WPI_VictorSPX(8);
+	WPI_VictorSPX *shooterangr = new WPI_VictorSPX(9);
+	WPI_VictorSPX *turnlb = new WPI_VictorSPX(10);
+	WPI_VictorSPX *turnrb = new WPI_VictorSPX(11);
+	WPI_VictorSPX *turnlf = new WPI_VictorSPX(12);
+	WPI_VictorSPX *turnrf = new WPI_VictorSPX(13);
+	Compressor *compressor = new Compressor();
+	Solenoid *solenoidrampl = new Solenoid(0);
+	Solenoid *solenoidrampr = new Solenoid(1);
+
+
 	/* put all declarations here */
 
 private:
