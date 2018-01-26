6d451867c539c8c7348e2a2c75ed09aacefb993f
Blzzrd <blzzrd_git@nym.hush.com>
2018-01-26 15:54:37 -0600

    Adds RobotCommands class
    Contains current commands for all robot functions

---
+++ src/RobotCommands.cpp
@@ -0,0 +1,18 @@
+/*
+ * RobotCommands.cpp
+ *
+ *  Created on: Jan 26, 2018
+ *      Author: blzzrd
+ */
+
+#include <RobotCommands.h>
+
+RobotCommands::RobotCommands() {
+
+	//TODO:Add all robot Commands here
+	intakecmd = false;
+
+}
+
+
+
