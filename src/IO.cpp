f78fd21788f81fff9d1543f73b8198658556c518
beneder <35406962+beneder@users.noreply.github.com>
2018-01-25 19:04:45 -0600

    I o (#4)
    * add i\o with errors
    
    * instantiated the motors
    
    * instantiated and declared all motors
    
    * fixes magic number
    
    * moves things around

--- src/IO.cpp
+++ src/IO.cpp
@@ -7,3 +7,35 @@
 
 #include "IO.h"
 
+IO::IO() {
+
+	drvlbmot = new WPI_VictorSPX(0);
+	drvrbmot  = new WPI_VictorSPX(1);
+	drvlfmot  = new WPI_VictorSPX(2);
+	drvrfmot  = new WPI_VictorSPX(3);
+	intakelmot = new WPI_VictorSPX(4);
+	intakermot = new WPI_VictorSPX(5);
+	shooterlmot = new WPI_VictorSPX(6);
+	shooterrmot = new WPI_VictorSPX(7);
+	shooteranglmot = new TalonSRX(8);
+	shooterangrmot = new TalonSRX(9);
+	turnlbmot = new WPI_VictorSPX(10);
+	turnrbmot = new WPI_VictorSPX(11);
+	turnlfmot = new WPI_VictorSPX(12);
+	turnrfmot = new WPI_VictorSPX(13);
+	compressor = new Compressor();
+	solenoidrampl = new Solenoid(0);
+	solenoidrampr = new Solenoid(1);
+	solenoidpoker = new Solenoid(2);
+	intakearmarticulation = new Solenoid(3);
+	encdrvlb = new Encoder(0, 1, false, Encoder::EncodingType::k4X);
+	encdrvlf = new Encoder(2, 3, false, Encoder::EncodingType::k4X);
+	encdrvrb = new Encoder(4, 5, false, Encoder::EncodingType::k4X);
+	encdrvrf = new Encoder(6, 7, false, Encoder::EncodingType::k4X);
+	steerencdrvlb = new AnalogPotentiometer(0,sweerveanglogvoltagetodegrees,0);
+	steerencdrvlf = new AnalogPotentiometer(1,sweerveanglogvoltagetodegrees,0);
+	steerencdrvrb = new AnalogPotentiometer(2,sweerveanglogvoltagetodegrees,0);
+	steerencdrvrf = new AnalogPotentiometer(3,sweerveanglogvoltagetodegrees,0);
+	cubesensor = new DigitalInput(8);
+
+}
--- src/IO.h
+++ src/IO.h
@@ -6,15 +6,12 @@
  */
 
 #ifndef SRC_IO_H_
-#define SRC_IO_H_
+#define SRC_IO_H_ 1
 
-#define NUM_ENCODERS 10 /* we have 10 encoders on the robot */
-#define NUM_SOLENOIDS 10 /*placeholder 10 */
-#define NUM_TALONS 4 /* we only use talons to control steering for the swerve drive */
-#define NUM_VICTORS 8 /* we use victorSPX's everywhere else */
 
 #include <WPILib.h>
-#include <ctre/Phoenix.h>
+#include "ctre/Phoenix.h"
+//#include "ctre/phoenix/MotorControl/CAN/WPI_VictorSPX.h"
 /*
  * Contains an instance of all sensors, motors, and actuators to be passed
  * throughout the entire program
@@ -27,14 +24,51 @@ public:
 		return instance;
 	}
 
+
+
 	/* These make sure the class can't be copied */
 	IO(IO const&) = delete;
 	void operator=(IO const&) = delete;
 
+	// The scaling to multiply the voltage by to get a meaningful unit 360 degrees / 5 volts = 72
+	const int sweerveanglogvoltagetodegrees = 72;
+
+	WPI_VictorSPX *drvlbmot;
+	WPI_VictorSPX *drvrbmot;
+	WPI_VictorSPX *drvlfmot;
+	WPI_VictorSPX *drvrfmot;
+	WPI_VictorSPX *intakelmot;
+	WPI_VictorSPX *intakermot;
+	WPI_VictorSPX *shooterlmot;
+	WPI_VictorSPX *shooterrmot;
+	TalonSRX *shooteranglmot;
+	TalonSRX *shooterangrmot;
+	WPI_VictorSPX *turnlbmot;
+	WPI_VictorSPX *turnrbmot;
+	WPI_VictorSPX *turnlfmot;
+	WPI_VictorSPX *turnrfmot;
+	Compressor *compressor;
+	Solenoid *solenoidrampl;
+	Solenoid *solenoidrampr;
+	Solenoid *solenoidpoker;
+	Solenoid *intakearmarticulation;
+	Encoder *encdrvlb;
+	Encoder *encdrvlf;
+	Encoder *encdrvrb;
+	Encoder *encdrvrf;
+	AnalogPotentiometer *steerencdrvlb;
+	AnalogPotentiometer *steerencdrvlf;
+	AnalogPotentiometer *steerencdrvrb;
+	AnalogPotentiometer *steerencdrvrf;
+	DigitalInput *cubesensor;
+
+
+
 	/* put all declarations here */
 
 private:
-	IO() {}
+	IO();
+
 };
 
 #endif /* SRC_IO_H_ */
