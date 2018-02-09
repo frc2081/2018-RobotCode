f364ea4a3b14f8cfcde0126a32c4b7953bf34093
Swiftshadow <swiftshadow34@gmail.com>
2018-02-08 19:34:16 -0600

    Talon shooter configuration (#18)
    * Adds talon configuration logic for one talon
    
    * Acts more like speed control than position control
    
    * Updates DriveSystem to keep the SwerveLib internal
    
    * Adds voltage ramping for the talons
    
    * It finally works

---
+++ src/CubeSystem/TalonConfiguration.h
@@ -0,0 +1,31 @@
+/*
+ * TalonConfiguration.h
+ *
+ *  Created on: Jan 26, 2018
+ *      Author: support
+ */
+
+#ifndef SRC_CUBESYSTEM_TALONCONFIGURATION_H_
+#define SRC_CUBESYSTEM_TALONCONFIGURATION_H_
+#include "IO.h"
+#include "RobotCommands.h"
+#include "CubeManagerIO.h"
+
+//temp
+#include <ControllerInput/cntl.h>
+class TalonConfiguration {
+public:
+	TalonConfiguration(IO *io);
+	virtual ~TalonConfiguration();
+
+	void TalonConfigInit();
+
+private:
+	IO *_io;
+	bool _invert; /* pick this based on your preference on what positive motor output should spin to */
+	bool _sensorphase; /* pick this so self-test stops reporting sensor-out-of-phase */
+	double _exchangeheight, _switchheight, _scaleheight;
+	double _maxtalonvolts, _ramptimeout;
+};
+
+#endif /* SRC_CUBESYSTEM_TALONCONFIGURATION_H_ */
