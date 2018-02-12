7231fd98b0aa78e5b8d4b34c4047f991932187a8
Blzzrd <blzzrd_git@nym.hush.com>
2018-02-11 20:59:04 -0600

    Integrates manual control into new cube control system

--- src/CubeSystem/CubeManager.cpp
+++ src/CubeSystem/CubeManager.cpp
@@ -30,6 +30,8 @@ void CubeManager::CubeManagerPeriodic(RobotCommands *Commands)
 	/*Chooses which state machine has control of the IO. If no state machine is in control,
 	keeps all outputs set to their last value*/
 
+	double curShooterAng = CubeManagerInput->getShooterAngleActualValue();
+
 	//Manual commands
 	if (Commands->cmdisinmechmanual) {
 		RioIO->shooteranglmot->Set(ControlMode::PercentOutput, Commands->cmdmanualshooterangleraise - Commands->cmdmanualshooteranglelower);
@@ -55,128 +57,146 @@ void CubeManager::CubeManagerPeriodic(RobotCommands *Commands)
 		}
 		RioIO->solenoidpoker->Set(Commands->cmdmanualshooterpoker);
 		manualarmsre = Commands->cmdmanualshooterarms;
-	}
-	switch(state)
-	{
-		case STATE::Idle:
-			if (Commands->cmdshiftcube && CubeManagerOutput->pokerpos == CubeManagerOutputs::PokerPosition::EXTENDED)
-				{ state = STATE::shifttohighcarry; }
-			else if (Commands->cmdshiftcube &&
-			CubeManagerOutput->pokerpos == CubeManagerOutputs::PokerPosition::RETRACTED)
-			{ state = STATE::Shifttolowcarry; }
-			else if ((Commands->cmdintakelowshot || Commands->cmdintakehighshot) &&
-			CubeManagerInput->getShooterCubeSensor() == CubeManagerInputs::CubeSensor::NO_CUBE_PRESENT )
-				{ state = STATE::Waitingforcube; }
-			else if (Commands->cmdscaleshot &&
-			CubeManagerInput->getShooterCubeSensor() == CubeManagerInputs::CubeSensor::CUBE_PRESENT &&
-			CubeManagerOutput->pokerpos == CubeManagerOutputs::PokerPosition::RETRACTED )
-				{ state = STATE::Highshotaimandspinup; }
-			else if ((Commands->cmdswitchshot || Commands->cmdexchangeshot) &&
-			CubeManagerInput->getShooterCubeSensor() == CubeManagerInputs::CubeSensor::CUBE_PRESENT &&
-			CubeManagerOutput->pokerpos == CubeManagerOutputs::PokerPosition::EXTENDED)
-				{ state = STATE::Lowshotaim; }
-			break;
-		case STATE::Waitingforcube:
-			CubeManagerOutput->shooteranglecmd = 0;
-			CubeManagerOutput->shooterArmPos = CubeManagerOutputs::ShooterArmPosition::OPEN;
-			if (Commands->cmdintakelowshot)
-			{
-				CubeManagerOutput->pokerpos = CubeManagerOutputs::PokerPosition::EXTENDED;
-			}else if (Commands->cmdintakehighshot)
-			{
-				CubeManagerOutput->pokerpos = CubeManagerOutputs::PokerPosition::RETRACTED;
-			}
-			CubeManagerOutput->intakepowercmd = -0.3;
-			CubeManagerOutput->shooterpowercmd = -0.3;
-			if (CubeManagerInput->getIntakeCubeSensor() == CubeManagerInputs::CubeSensor::CUBE_PRESENT)
-			{
-				state = STATE::Intakingcube;
-			}
-			break;
-		case STATE::Intakingcube:
-			CubeManagerOutput->shooteranglecmd = 0;
-			CubeManagerOutput->shooterArmPos = CubeManagerOutputs::ShooterArmPosition::CLOSED;
-			CubeManagerOutput->intakepowercmd = -0.3;
-			CubeManagerOutput->shooterpowercmd = -0.3;
-			--intakeexittimer;
-			if (intakeexittimer == 0 ||
-			CubeManagerInput->getShooterCubeSensor() == CubeManagerInputs::CubeSensor::CUBE_PRESENT)
-			{
-				state = STATE::Idle;
-				intakeexittimer =100;
-			}
-			break;
-		case STATE::Highshotaimandspinup:
-			CubeManagerOutput->shooteranglecmd = 75;
-			CubeManagerOutput->intakepowercmd = 1;
-			CubeManagerOutput->shooterpowercmd = 1;
-			CubeManagerOutput->shooterArmPos = CubeManagerOutputs::ShooterArmPosition::CLOSED;
-			--highshotentertimer;
-			if ((int)CubeManagerInput->getShooterAngleActualValue() == (int)CubeManagerOutput->shooteranglecmd &&
-			highshotentertimer == 0)
-			{
-				highshotentertimer = 200;
-				state = STATE::Highshot;
-			}
-			break;
-		case STATE::Highshot:
-			CubeManagerOutput->pokerpos = CubeManagerOutputs::PokerPosition::EXTENDED;
-			--highshotexittimer;
-			if(highshotexittimer == 0)
-			{
-				state = STATE::Idle;
-				highshotexittimer = 100;
-			}
-			break;
-		case STATE::Lowshotaim:
-			if(Commands->cmdswitchshot)
-			{
-				CubeManagerOutput->shooteranglecmd = 35;
-			}else if (Commands->cmdexchangeshot)
-			{
-				CubeManagerOutput->shooteranglecmd = 5;
-			}
-			CubeManagerOutput->shooterArmPos = CubeManagerOutputs::ShooterArmPosition::CLOSED;
-			if((int)CubeManagerInput->getShooterAngleActualValue() == (int)CubeManagerOutput->shooteranglecmd)
-			{
-				state = STATE::Lowshot;
-			}
-			break;
-		case STATE::Lowshot:
-			CubeManagerOutput->intakepowercmd = 0.5;
-			CubeManagerOutput->shooterpowercmd = 0.5;
-			--lowshotexittimer;
-			if(lowshotexittimer == 0)
-			{
-				lowshotexittimer = 200;
-				state = STATE::Idle;
-			}
-			break;
+	} else {
+		switch(state)
+		{
+			case STATE::Idle:
+				CubeManagerOutput->shooteranglecmd = shooterHomeAngle;
+				CubeManagerOutput->shooterpowercmd = 0;
+				CubeManagerOutput->intakepowercmd = 0;
 
-		case STATE::Shifttolowcarry:
-			CubeManagerOutput->pokerpos = CubeManagerOutputs::PokerPosition::EXTENDED;
-			if(CubeManagerOutput->pokerpos == CubeManagerOutputs::PokerPosition::EXTENDED)
-			{
-				state = STATE::Idle;
-			}
-			break;
+				if (Commands->cmdshiftcube && CubeManagerOutput->pokerpos == CubeManagerOutputs::PokerPosition::EXTENDED)
+					{ state = STATE::shifttohighcarry; }
+				else if (Commands->cmdshiftcube && CubeManagerOutput->pokerpos == CubeManagerOutputs::PokerPosition::RETRACTED)
+					{ state = STATE::Shifttolowcarry; }
+				else if ((Commands->cmdintakelowshot || Commands->cmdintakehighshot) && CubeManagerInput->getShooterCubeSensor() == CubeManagerInputs::CubeSensor::NO_CUBE_PRESENT )
+					{ state = STATE::Waitingforcube; }
+				else if (Commands->cmdscaleshot && CubeManagerInput->getShooterCubeSensor() == CubeManagerInputs::CubeSensor::CUBE_PRESENT && CubeManagerOutput->pokerpos == CubeManagerOutputs::PokerPosition::RETRACTED )
+					{ state = STATE::Highshotaimandspinup; }
+				else if ((Commands->cmdswitchshot || Commands->cmdexchangeshot) && CubeManagerInput->getShooterCubeSensor() == CubeManagerInputs::CubeSensor::CUBE_PRESENT && CubeManagerOutput->pokerpos == CubeManagerOutputs::PokerPosition::EXTENDED)
+					{ state = STATE::Lowshotaim; }
 
-
-		case STATE::shifttohighcarry:
-			CubeManagerOutput->intakepowercmd = shiftPower;
-			CubeManagerOutput->shooterpowercmd = shiftPower;
-
-			--shifttimer;
-			CubeManagerOutput->pokerpos = CubeManagerOutputs::PokerPosition::RETRACTED;
-			if(shifttimer <= shiftDuration)
-			{
+				highshotentertimer = highShotSpinUpDelay;
+				highshotexittimer = highShotShotDuration;
+				lowshotexittimer = lowShotShotDuration;
 				shifttimer = shiftDuration;
 
-				state = STATE::Idle;
-			}
-			break;
-	}
+				break;
 
+			case STATE::Waitingforcube:
+				CubeManagerOutput->shooteranglecmd = shooterCubePickupAngle;
+				CubeManagerOutput->shooterArmPos = CubeManagerOutputs::ShooterArmPosition::OPEN;
+				CubeManagerOutput->intakepowercmd = armsCubeIntakePower;
+				CubeManagerOutput->shooterpowercmd =  shooterCubeIntakePower;
+
+				if(Commands->cmdintakelowshot == false && Commands->cmdintakehighshot == false) {
+					state = STATE::Idle;
+				}else if (Commands->cmdintakelowshot) {
+					CubeManagerOutput->pokerpos = CubeManagerOutputs::PokerPosition::EXTENDED;
+				} else if (Commands->cmdintakehighshot) {
+					CubeManagerOutput->pokerpos = CubeManagerOutputs::PokerPosition::RETRACTED;
+				}
+
+				if (CubeManagerInput->getIntakeCubeSensor() == CubeManagerInputs::CubeSensor::CUBE_PRESENT) {
+					state = STATE::Intakingcube;
+				}
+				break;
+
+			case STATE::Intakingcube:
+				CubeManagerOutput->shooteranglecmd = shooterCubePickupAngle;
+				CubeManagerOutput->shooterArmPos = CubeManagerOutputs::ShooterArmPosition::CLOSED;
+				CubeManagerOutput->intakepowercmd = armsCubeIntakePower;
+				CubeManagerOutput->shooterpowercmd = shooterCubeIntakePower;
+
+				if(Commands->cmdintakelowshot == false && Commands->cmdintakehighshot == false) {
+					state = STATE::Idle;
+				} else if (Commands->cmdintakelowshot == true && CubeManagerInput->getShooterCubeSensor() == CubeManagerInputs::CubeSensor::CUBE_PRESENT){
+					state = STATE::Idle;
+				}
+				break;
+
+			case STATE::Highshotaimandspinup:
+				CubeManagerOutput->shooteranglecmd = highShotAimAngle;
+				CubeManagerOutput->intakepowercmd = highShotShooterPower;
+				CubeManagerOutput->shooterpowercmd = highShotIntakePower;
+				CubeManagerOutput->shooterArmPos = CubeManagerOutputs::ShooterArmPosition::CLOSED;
+
+				--highshotentertimer;
+
+				if(Commands->cmdscaleshot == false) {state = STATE::Idle;}
+				else if ((curShooterAng > highShotAimAngle - highShotAimMargin && curShooterAng < highShotAimAngle + highShotAimMargin) && highshotentertimer <= 0){
+					highshotentertimer = highShotSpinUpDelay;
+					state = STATE::Highshot;
+				}
+				break;
+
+			case STATE::Highshot:
+				CubeManagerOutput->pokerpos = CubeManagerOutputs::PokerPosition::EXTENDED;
+				--highshotexittimer;
+				if(highshotexittimer <= 0){
+					state = STATE::Idle;
+					highshotexittimer = highShotShotDuration;
+				}
+				break;
+
+			case STATE::Lowshotaim:
+				CubeManagerOutput->shooterArmPos = CubeManagerOutputs::ShooterArmPosition::CLOSED;
+				if(Commands->cmdswitchshot == false && Commands->cmdexchangeshot == false) {
+					state = STATE::Idle;
+				} else if(Commands->cmdswitchshot) {
+					CubeManagerOutput->shooteranglecmd = lowShotAimAngle;
+				}else if (Commands->cmdexchangeshot) {
+					CubeManagerOutput->shooteranglecmd = exchangeShotAimAngle;
+				}
+
+				if(Commands->cmdswitchshot && (curShooterAng > lowShotAimAngle - lowShotAimMargin && curShooterAng < lowShotAimAngle + lowShotAimMargin)) {
+					state = STATE::Lowshot;
+				}
+				break;
+
+			case STATE::Lowshot:
+
+				--lowshotexittimer;
+				if(Commands->cmdswitchshot)
+				{
+					CubeManagerOutput->intakepowercmd = lowShotIntakePower;
+					CubeManagerOutput->shooterpowercmd = lowShotShooterPower;
+					if(lowshotexittimer <= 0){
+						lowshotexittimer = lowShotShotDuration;
+						state = STATE::Idle;
+					}
+				} else if (Commands->cmdexchangeshot) {
+					CubeManagerOutput->intakepowercmd = exchangeShotIntakePower;
+					CubeManagerOutput->shooterpowercmd = exchangeShotShooterPower;
+					if(lowshotexittimer <= 0){
+						lowshotexittimer = exchangeShotDuration;
+						state = STATE::Idle;
+					}
+				}
+				break;
+
+			case STATE::Shifttolowcarry:
+				CubeManagerOutput->pokerpos = CubeManagerOutputs::PokerPosition::EXTENDED;
+				if(CubeManagerOutput->pokerpos == CubeManagerOutputs::PokerPosition::EXTENDED)
+				{
+					state = STATE::Idle;
+				}
+				break;
+
+			case STATE::shifttohighcarry:
+				CubeManagerOutput->intakepowercmd = shiftPower;
+				CubeManagerOutput->shooterpowercmd = shiftPower;
+
+				--shifttimer;
+				CubeManagerOutput->pokerpos = CubeManagerOutputs::PokerPosition::RETRACTED;
+				if(shifttimer <= shiftDuration)
+				{
+					shifttimer = shiftDuration;
+					state = STATE::Idle;
+				}
+				break;
+		}
+	}
 }
 
 
